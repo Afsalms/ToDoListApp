459754b289f62c94c8a7db8c92b97ed89bc90dc2
Afsalms <afsalms93.mec@gmail.com>
2018-12-15 14:28:33 +0530

    Added click listener for the buttons

--- app/src/main/java/com/example/afsal/todolistapp/MainActivity.kt
+++ app/src/main/java/com/example/afsal/todolistapp/MainActivity.kt
@@ -2,11 +2,49 @@ package com.example.afsal.todolistapp
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener {
+
+    var numberOfItem = 0
+
+
+    override fun onClick(v: View?) {
+        println("Clicked")
+        when(v){
+            increment -> {
+                println("increment button clicked")
+                numberOfItem ++
+                textView.text = numberOfItem.toString()
+
+            }
+            decrement -> {
+                println("Decrement button clicked")
+                if (numberOfItem > 0){
+                    numberOfItem --
+                    textView.text = numberOfItem.toString()
+                }
+            }
+            addbutton -> {
+                println("Add button clicked")
+                numberOfItem = 0
+                editText.text = null
+                textView.text = numberOfItem.toString()
+            }
+        }
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        increment.setOnClickListener(this)
+        decrement.setOnClickListener(this)
+        addbutton.setOnClickListener(this)
+
+
+
+
     }
 }
